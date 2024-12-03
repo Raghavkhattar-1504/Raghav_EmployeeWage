0124933e1dd4e94c19110220f3b4a1d19c67c309
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2024-12-03 19:08:35 +0530

    [raghav]: Refactor - Employee wage for no time added

--- employeewage.js
+++ employeewage.js
@@ -8,5 +8,19 @@ else {
     console.log("Employee is Absent");
 }
 
+// UC2
+const timing = Math.floor(Math.random()*3);
 
+switch (timing) {
+    case 0:
+        console.log("The employee wage for full day is $160");
+        break;
+    case 1:
+        console.log("The employee wage for half day is $80");
+        break;
+    case 2:
+        console.log("The employee wage for no working is $0");
+    default:
+        break;
+}
 
