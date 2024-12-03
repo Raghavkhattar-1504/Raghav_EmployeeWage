96b645873c921a1f33f7199bbce1b6507c9ad501
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2024-12-03 19:16:32 +0530

    [raghav]: Refactor - Encapsulate the wage calculation into a function

--- employeewage.js
+++ employeewage.js
@@ -9,7 +9,24 @@ else {
 }
 
 // UC2
-const timing = Math.floor(Math.random()*3);
+// const timing = Math.floor(Math.random()*3);
+
+// switch (timing) {
+//     case 0:
+//         console.log("The employee wage for full day is $160");
+//         break;
+//     case 1:
+//         console.log("The employee wage for half day is $80");
+//         break;
+//     case 2:
+//         console.log("The employee wage for no working is $0");
+//     default:
+//         break;
+// }
+
+// UC3
+function wageCalculator() {
+    const timing = Math.floor(Math.random()*3);
 
 switch (timing) {
     case 0:
@@ -23,4 +40,6 @@ switch (timing) {
     default:
         break;
 }
+}
 
+wageCalculator();
