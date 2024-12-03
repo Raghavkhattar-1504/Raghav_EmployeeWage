210eb8dabe3b56f4d85def11a7684edbbb340763
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2024-12-03 19:28:46 +0530

    [raghav]: Refractor - Total salary

--- employeewage.js
+++ employeewage.js
@@ -1,5 +1,5 @@
 // UC1
-const attendance = Math.floor(Math.random()*2);
+const attendance = Math.floor(Math.random() * 2);
 
 if (attendance === 1) {
     console.log("Employee is Present");
@@ -26,20 +26,30 @@ else {
 
 // UC3
 function wageCalculator() {
-    const timing = Math.floor(Math.random()*3);
-
-switch (timing) {
-    case 0:
-        console.log("The employee wage for full day is $160");
-        break;
-    case 1:
-        console.log("The employee wage for half day is $80");
-        break;
-    case 2:
-        console.log("The employee wage for no working is $0");
-    default:
-        break;
+    const timing = Math.floor(Math.random() * 3);
+    let wage = 0;
+    switch (timing) {
+        case 0:
+            console.log("The employee wage for full day is $160");
+            wage = 160;
+            break;
+        case 1:
+            console.log("The employee wage for half day is $80");
+            wage = 80;
+            break;
+        case 2:
+            console.log("The employee wage for no working is $0");
+        default:
+            break;
+    }
+    return wage;
 }
-}
 
-wageCalculator();
+// wageCalculator();
+
+
+// UC4
+
+const totalSalary = wageCalculator();
+console.log(`Total Salary for the month is ${20*totalSalary}`);
+
