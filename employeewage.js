55687dd670dc5b1870b423498008eba86b1182f8
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2024-12-03 20:00:46 +0530

    [Raghav]: Refractor - Total salary for month

--- employeewage.js
+++ employeewage.js
@@ -30,15 +30,15 @@ function wageCalculator() {
     let wage = 0;
     switch (timing) {
         case 0:
-            console.log("The employee wage for full day is $160");
+            // console.log("The employee wage for full day is $160");
             wage = 160;
             break;
         case 1:
-            console.log("The employee wage for half day is $80");
+            // console.log("The employee wage for half day is $80");
             wage = 80;
             break;
         case 2:
-            console.log("The employee wage for no working is $0");
+            // console.log("The employee wage for no working is $0");
         default:
             break;
     }
@@ -53,3 +53,29 @@ function wageCalculator() {
 const totalSalary = wageCalculator();
 console.log(`Total Salary for the month is ${20*totalSalary}`);
 
+
+// UC5
+
+let days = 0;
+let totalhours = 0;
+let salary = 0;
+while (days < 20 && totalhours < 160) {
+    let empstatus = Math.floor(Math.random()*2);
+    switch (empstatus) {
+        case 0:
+            days++;
+            totalhours += 8;
+            salary += (8*20);
+            break;
+        case 1:
+            days++;
+            totalhours += 4;
+            salary += (4*20);
+            break;
+        default:
+            break;
+    }
+}
+
+console.log(totalhours);
+console.log(salary);
